2207f50b0583ab177b8a83a547582b2ebaa7340a
Revathy <revathy_elangovan@comcast.com>
2017-11-09 13:50:08 -0500

    Added send_req and send_res sample apps to CMakeList

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/revathy/Revathy/rtmessage
+# Install script for directory: /home/revathy/Revathy/github/9Nov/rtMessage
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/revathy/Revathy/rtmessage/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/revathy/Revathy/github/9Nov/rtMessage/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
